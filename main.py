86d30405c582e4e6ef59ced7730e118cfc74befa
Gayan Sandamal <gayansandamal94@gmail.com>
2024-05-08 11:24:54 +0530

    text to image 2

--- main.py
+++ main.py
@@ -1,48 +1,20 @@
 from typing import Union
-
 from fastapi import FastAPI
+import torch
+from diffusers import StableDiffusionPipeline
 
 app = FastAPI()
 
-from diffusers import DiffusionPipeline
-import torch
-
-# load both base & refiner
-base = DiffusionPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
-)
-base.to("cuda")
-refiner = DiffusionPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-refiner-1.0",
-    text_encoder_2=base.text_encoder_2,
-    vae=base.vae,
-    torch_dtype=torch.float16,
-    use_safetensors=True,
-    variant="fp16",
-)
-refiner.to("cuda")
+pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
 
 # Define how many steps and what % of steps to be run on each experts (80/20) here
 n_steps = 40
 high_noise_frac = 0.8
 
 def generate_image(prompt):
-  # run both experts
-  image = base(
-      prompt=prompt,
-      num_inference_steps=n_steps,
-      denoising_end=high_noise_frac,
-      output_type="latent",
-  ).images
-  image = refiner(
-      prompt=prompt,
-      num_inference_steps=n_steps,
-      denoising_start=high_noise_frac,
-      image=image,
-  ).images[0]
-
-  image.show()
-  display(image)
+    image = pipe(prompt).images[0]
+    display(image)
 
 def generate_images(prompt, copies_of_images = 1):
   i = 0
@@ -60,4 +32,4 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.get("/generate/{prompt}")
 def read_item(prompt: str, q: Union[str, None] = None):
-    return generate_images(prompt)
+    return generate_images(prompt, 3)
